9a135da7166e7c85ebfbb4be25f5d8e69c28e7b3
estuardoMC <estuarditochez@gmail.com>
2022-08-08 17:06:22 -0600

    importar estilos de manera global

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'//importacion de react
 import ReactDOM from 'react-dom/client'//react dom
 import { FirstApp } from './FirstApp'
-import { App } from './HelloWordlApp'//importamos nuestro componente 
-
+//import { App } from './HelloWordlApp'//importamos nuestro componente 
+import './estilo.css';//importamos estilos que afecten de manera global para eso lo importamos aqui en el main.jsx
 //creamos una raiz y luego renderizamos la aplicacion
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
