b75d293c3bc91c23b6402c9facf2ca567fc1d261
estuardoMC <estuarditochez@gmail.com>
2022-08-08 17:25:04 -0600

    mandar a llamar componente FirstApp mandandole props

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -7,10 +7,11 @@ arrN.forEach((n)=>{
 const returnN = (n1, n2)=>{
     return n1 + n2;
 }
-export const FirstApp = ()=>{
+export const FirstApp = ({title, numero})=>{//los props los podemos ver como parametros que recibe el componente de donde se manda a llamar
     //imprimir variables en el jsx podemos utilizar el valor de una cariable media vez no sea object
     return(//no podemos retornar varios elementos html entonces debemos encerrar todo dentro de un div
         <>
+        <h1>{title +" "+ numero}</h1>
         <h1>{name}</h1>
         <p>{stringN}</p>
         <p>{arrN}</p>
