7078f408b6b3add1adf87fcc1c26f3f29dca074c
estuardoMC <estuarditochez@gmail.com>
2022-08-09 16:36:26 -0600

    añadiendo un evento click al boton

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -4,6 +4,9 @@ export const CounterApp =({value})=>{
         <>
             <h1>CounterApp</h1>
             <h2> { value } </h2>
+            <button onClick={ ()=>{console.log("hola")} /*los eventos click se declaran dentro del elemento html*/}>
+                +1
+            </button>
         </>
     )
 }
