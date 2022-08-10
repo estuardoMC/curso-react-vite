9beda811f1907ec19dbb666948eddb8a2900bd00
estuardoMC <estuarditochez@gmail.com>
2022-08-10 16:46:54 -0600

    utilizar valor de props como valor inicial del state

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'; //importando un hook useState
 import PropTypes from 'prop-types';
-export const CounterApp =({value})=>{
-    const [ counter, setCounter ] = useState( 0 ); //desestructuramos lo que viene de la funcion useState(), y una funcion para modificar el state
+export const CounterApp =({value})=>{//podemos usar el valor que desestructuramos de los props para usarlo como valor inicial del state
+    const [ counter, setCounter ] = useState( value ); //desestructuramos lo que viene de la funcion useState(), y una funcion para modificar el state
     const handleAdd = (e) =>{
         //setCounter( counter + 1 );//esta funicon la llamamos mandandole el state modificado 
         //desde la version 18 de react solo se va a actualizar el estado una vez que haya terminado toda la funcion que llama al setCouter
