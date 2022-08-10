e41521c590d31c9a1c19b85630b794a2a75eec31
estuardoMC <estuarditochez@gmail.com>
2022-08-10 16:43:28 -0600

    forma 2 de modificar el state

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 export const CounterApp =({value})=>{
     const [ counter, setCounter ] = useState( 0 ); //desestructuramos lo que viene de la funcion useState(), y una funcion para modificar el state
     const handleAdd = (e) =>{
-        setCounter( counter + 1 );//esta funicon la llamamos mandandole el state modificado 
+        //setCounter( counter + 1 );//esta funicon la llamamos mandandole el state modificado 
         //desde la version 18 de react solo se va a actualizar el estado una vez que haya terminado toda la funcion que llama al setCouter
+        //otra forma de modificar el valor del state es mandando a llamar el setCouter mandando un callback que retorna el valor del state y despues podemos modificarlo
+        setCounter((s) => s + 1)
     }
     return (
         <>
