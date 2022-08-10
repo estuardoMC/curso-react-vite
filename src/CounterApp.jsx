508cb9bb5927f7f74e329d2774efaf9935d3ab13
estuardoMC <estuarditochez@gmail.com>
2022-08-10 16:40:17 -0600

    importando hook useState forma 1 de modificar el state

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,13 +1,15 @@
+import { useState } from 'react'; //importando un hook useState
 import PropTypes from 'prop-types';
 export const CounterApp =({value})=>{
-
+    const [ counter, setCounter ] = useState( 0 ); //desestructuramos lo que viene de la funcion useState(), y una funcion para modificar el state
     const handleAdd = (e) =>{
-        console.log(e);
+        setCounter( counter + 1 );//esta funicon la llamamos mandandole el state modificado 
+        //desde la version 18 de react solo se va a actualizar el estado una vez que haya terminado toda la funcion que llama al setCouter
     }
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
             <button onClick={ handleAdd } /*recibimos el e y lo mandamos a la funcion*/>
                 +1
             </button>
