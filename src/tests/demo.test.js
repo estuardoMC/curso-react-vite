e1554ddf212ad0f900a23ad78890003cf83645dd
estuardoMC <estuarditochez@gmail.com>
2022-08-16 16:10:15 -0600

    crear prueba con nombre y usando condicional propia de jest

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,5 +1,10 @@
 test('Esta prueba no debe de fallar ', ()=>{ //la funcion test lleva como parametro el nombre de la prueba y un callback donde se ejecutara la prueba
-    if(1 === 0){//dentro de la prueba se evaluara cierta condicion si se cumple la funcion pasara analizada 
-        throw new Error("no se puede dividir entre cero")
-    }
+    //1) inicializacion 
+    const message1 = "hola mundo";
+
+    // 2) estimulo 
+    const message2 = message1.trim();
+
+    //3) observar el comportamiento... esperando
+    expect( message1 ).toBe( message2 );//condicion: con esto decimos que message1 debe ser igual a meesage2
 });
